9c60e9c46fa5032a82e2caad7ec2c745e58575a7
ankit <ankitshriramwar2821@gmail.com>
2024-05-17 23:27:41 +0530

    intial comit

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log("Hi Ankit");
+
+name = "Ankit";
+
+// console.log(typeof name);
+
+let x = 2;
+let y = x++;
+
+// console.log(x, y);
+
+// console.log(null == undefined); //true
+// console.log(null >= undefined); //false
+// console.log(null > undefined); //false
+// console.log(null === undefined); //false
+
+// console.log(null >= 0); //true
+// console.log(null == 0); //false
+// console.log(null > 0); //false
+// console.log(null === 0); //false
+
+// console.log("33" == 33); //true
+// console.log("33" === 33); //false
+
+console.log("" == null);
+console.log("" === null);
+console.log("" == undefined);
+console.log("" == undefined);
