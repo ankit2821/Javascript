f10a5e2744eabf1cf94ae33e378273de8bc5d3a0
ankit <ankitshriramwar2821@gmail.com>
2024-05-22 00:12:41 +0530

    array concepts revised

---
+++ basic/ArrayException.js
@@ -0,0 +1,11 @@
+console.log(Array.isArray("Ankit"));
+
+console.log(Array.from("Ankit"));
+
+console.log(Array.from({ name: "Ankit" })); //this actually return a empty array!!
+
+let name = "ankit";
+let name2 = "mouse";
+let name3 = "keyboard";
+
+console.log(Array.of(name, name2, name3));
---
+++ basic/Slice_and_Splice.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, 4, 5, 6, 8];
+
+// slice_arr = arr.slice(1, 5);
+// console.log("Sliced Array: ", slice_arr);
+// console.log("original arr: ", arr);
+
+splice_arr = arr.splice(1, 4);
+
+console.log("Spliced Array: ", splice_arr);
+console.log("original arr: ", arr);
