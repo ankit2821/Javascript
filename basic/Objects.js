2ffb8edf8d81161585e98235d095ee47183cfe19
ankit <ankitshriramwar2821@gmail.com>
2024-05-24 23:49:15 +0530

    function

--- basic/Objects.js
+++ basic/Objects.js
@@ -1,24 +1,24 @@
-// let mySym = Symbol("mySym");
+let mySym = Symbol("mySym");
 
-// const muObj = {
-//   name: "Ankit",
-//   age: 20,
-//   [mySym]: "qwertyu",
-//   greeting: function () {
-//     console.log("Hi Ankit");
-//   },
-//   userFullnaem: {
-//     fullnaem: {
-//       first_name: "AnkitPs",
-//     },
-//   },
-// };
-
+const muObj = {
+  name: "Ankit",
+  age: 20,
+  [mySym]: "qwertyu",
+  greeting: function () {
+    console.log(this.name);
+  },
+  userFullnaem: {
+    fullnaem: {
+      first_name: "ankit",
+    },
+  },
+};
+muObj.greeting();
 // console.log(muObj.userFullname?.fullnaem.first_name);
+// const myname = Symbol("name ");
+// const obj1 = { name: "ankit", [myname]: "werty" };
+// const obj2 = { age: 23 };
 
-const obj1 = { name: "ankit" };
-const obj2 = { age: 23 };
+// const obj = { ...obj1, ...obj2 };
 
-const obj = { ...obj1, ...obj2 };
-
-console.log(obj);
+// console.log(obj1);
---
+++ basic/functions.js
@@ -0,0 +1,26 @@
+// const myArr = [100, 200, 300, 400];
+
+// function arrayValue(arr) {
+//   return arr[3];
+// }
+
+// console.log(arrayValue(myArr));
+
+////used in props
+// const myObj = {
+//   name: "Ankit",
+//   age: 24,
+//   company: "Cognizant",
+// };
+
+// function objectValue(obj) {
+//   return obj.age;
+// }
+
+// console.log(objectValue(myObj));
+
+// function returnMultipleValue(...value) {
+//   return value;
+// }
+
+// console.log(returnMultipleValue(100, 200, 3, 0, 400));
