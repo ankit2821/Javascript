703b42789fa94b99a2fb4c33c43b758a419dc8a4
ankit <ankitshriramwar2821@gmail.com>
2024-05-27 22:42:19 +0530

    how to checck a array or object is empty or not

---
+++ basic/checks_for_arr_and_Obj.js
@@ -0,0 +1,18 @@
+let arr = [];
+let obj = {};
+
+if (arr) {
+  console.log("Array not empty");
+}
+
+if (arr.length === 0) {
+  console.log("Empty arrays");
+}
+
+if (obj) {
+  console.log("object not Empty");
+}
+
+if (Object.keys(obj).length === 0) {
+  console.log("Empty Object");
+}
