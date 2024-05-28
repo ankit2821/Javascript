ce35957fd18fa7c26db0bf16a3c4c6ec8a1b739c
ankit <ankitshriramwar2821@gmail.com>
2024-05-28 23:26:05 +0530

    filter done

---
+++ basic/filter.js
@@ -0,0 +1,35 @@
+const arr = [
+  {
+    mode: "Vivo",
+    ram: "6GB",
+    date: 2022,
+  },
+  {
+    mode: "Oppo",
+    ram: "4GB",
+    date: 2021,
+  },
+  {
+    mode: "Iqooo",
+    ram: "8GB",
+    date: 2023,
+  },
+  {
+    mode: "Apple",
+    ram: "6GB",
+    date: 2022,
+  },
+  {
+    mode: "Samsung",
+    ram: "4GB",
+    date: 2023,
+  },
+  {
+    mode: "One plus",
+    ram: "6GB",
+    date: 2023,
+  },
+];
+
+const mobiles = arr.filter((mobile) => mobile.date < 2023);
+console.log(mobiles);
