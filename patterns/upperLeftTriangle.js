4ac60579529d26a6d8eaefc950f2f339bd25dad7
ankit <ankitshriramwar2821@gmail.com>
2024-06-08 15:52:50 +0530

    using three different approach to print starts in triangle

---
+++ patterns/upperLeftTriangle.js
@@ -0,0 +1,25 @@
+// ++++++++++++  with nested loops ++++++++++++++++++++++++
+
+// let n = 5;
+// for (let i = 0; i <= n; i++) {
+//   box = "";
+//   for (let j = 0; j <= i; j++) {
+//     box += "* ";
+//   }
+//   console.log(box);
+// }
+
+// ++++++++  using repeat function +++++++++++++++++++++++++++
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let res = "* ";
+//   console.log(res.repeat(i));
+// }
+
+// ++++++++++++++++++++++++++++using array, fill , join method++++++++++++++++++++++
+
+// let n = 5;
+// for (let i = 1; i <= 5; i++) {
+//   console.log(Array(i).fill("*").join(" "));
+// }
